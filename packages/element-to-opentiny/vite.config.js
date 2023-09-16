1e8c500e44c90170d6acd6beb2cb5ce03deef4c7
Kagol <kagol@sina.com>
2023-09-16 08:06:21 +0800

    refactor: add @vitejs/plugin-vue

--- packages/element-to-opentiny/vite.config.js
+++ packages/element-to-opentiny/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
-import { createVuePlugin } from 'vite-plugin-vue2'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [createVuePlugin()],
+  plugins: [vue()],
   server: {
     port: 2200,
     host: 'localhost'
